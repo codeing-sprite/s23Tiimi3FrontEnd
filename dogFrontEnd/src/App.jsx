98aa783fd9612a19f18b6b78172527cf72e21400
Heinonen Tuukka <bgz855@myy.haaga-helia.fi>
2023-11-01 12:45:13 +0200

    add router and img

--- dogFrontEnd/src/App.jsx
+++ dogFrontEnd/src/App.jsx
@@ -1,11 +1,45 @@
 import './App.css'
 import PrductContainer from './PrductContainer'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom"
+
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <div>
+          <div className='nav-bar'>
+            <Link to="/">Doggystore</Link>
+            <Link to="about">About Us</Link>
+          </div>
+          <PrductContainer />
+        </div>
+      ),
+    },
+    {
+      path: "about",
+      element: (
+        <div>
+          <div className='nav-bar'>
+            <Link to="/">Doggystore</Link>
+            <Link to="about">About Us</Link>
+          </div>
+          <div>About</div>
+        </div>
+      ),
+    },
+  ]);
+  
   return (
     <>
-      <PrductContainer />
+      <RouterProvider router={router} />
     </>
   )
 }
--- dogFrontEnd/src/Product.jsx
+++ dogFrontEnd/src/Product.jsx
@@ -2,6 +2,7 @@ export default function Product({name}) {
 
     return (
       <>
+        <img src="../images/208645545_max.jpg" width={'197px'}></img>
         <h1>{name}</h1>
       </>
     )
