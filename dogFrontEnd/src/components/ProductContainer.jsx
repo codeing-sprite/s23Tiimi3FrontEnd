938d2a11c6f9352ccafe913adfd57d297246e694
Tukkis <54272662+Tukkis@users.noreply.github.com>
2023-11-18 19:30:12 +0200

    Add manufacturer states and selector

--- dogFrontEnd/src/components/ProductContainer.jsx
+++ dogFrontEnd/src/components/ProductContainer.jsx
@@ -7,10 +7,23 @@ export default function ProductContainer() {
 	//todo init default
 	const [product, setProduct] = useState(false);
 	const [products, setProducts] = useState([]);
-
+	const [manufacturer, setManufacturer] = useState("Select manufacturer")
+	const [manufacturers, setManufacturers] = useState([])
+	
 	const REST_URL = 'http://localhost:8080';
 
-	useEffect(() => listProducts(), [])
+	useEffect(() => {
+		listProducts()
+		listManufacturers()
+	}, [])
+
+	const listManufacturers = () => {
+		axios.get(`${REST_URL}/rest/manufacturers`)
+			.then((response) => {
+				setManufacturers(response.data);
+				console.log(response.data);
+			})
+	}
 
 	const listProducts = () => {
 		axios.get(`${REST_URL}/rest/items`)
@@ -20,14 +33,39 @@ export default function ProductContainer() {
 			})
 	}
 
+	const listManufacturersProducts = (manufacturerId) => {
+		axios.get(`${REST_URL}/rest/manufacturersitems/${manufacturerId}`)
+			.then((response) => {
+				setProducts(response.data);
+				console.log(response.data);
+			})
+	}
+
+	const handleManufacturerChange = (e) => {
+		console.log(e.target.value)
+		setManufacturer(e.target.value)
+		if (e.target.value === "Select manufacturer"){
+			listManufacturers()
+		} else {
+			listManufacturersProducts(e.target.value)
+		}
+	}
+
 	const renderProducts = products.map(item =>
 		<Product key={item.id} item={item} setProduct={setProduct} />
+	)
+
+	const renderManufacturers = manufacturers.map(item => 
+		<option key={item.id} value={item.id}>{item.name}</option>
 	)
 
 	return (
 		<div className='product-list'>
 			{renderProducts}
-			
+		<select value={manufacturer} onChange={(e) => handleManufacturerChange(e)}>
+			<option value={"Select manufacturer"}>Select manufacturer</option>
+			{renderManufacturers}
+		</select>
 		</div>
 	)
 }
